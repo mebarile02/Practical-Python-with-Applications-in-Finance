95804fa9106af4ad62328541a9c72d88a44f90b6
Michael Barile <33465350+mebarile02@users.noreply.github.com>
2020-12-24 17:32:54 -0500

    Add files via upload

---
+++ Chapter7/Assets/housebase.py
@@ -0,0 +1,30 @@
+from Assets.asset import Asset
+
+
+class HouseBase(Asset):
+    def __init__(self, ival, rate):
+        self._ival = ival
+        self._rate = rate
+        super(HouseBase, self).__init__(ival, rate)
+
+    def annualDepRate(self):
+        return self._rate
+
+class PrimaryHome(HouseBase):
+    def __init__(self, ival, rate=.03):
+        self._ival = ival
+        self._rate = rate
+        super(PrimaryHome, self).__init__(ival, rate)
+
+    def toString(self):
+        return 'Primary Home'
+
+
+class VacationHome(HouseBase):
+    def __init__(self, ival, rate=.02):
+        self._ival = ival
+        self._rate = rate
+        super(VacationHome, self).__init__(ival, rate)
+
+    def toString(self):
+        return 'Vacation Home'
